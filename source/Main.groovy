d1e4df8334ce9013338c085c9fbde343d3b48c63
Other Person <matthew@github.com>
2013-02-21 15:35:58 -0800

    Feature work

--- source/Main.groovy
+++ source/Main.groovy
@@ -11,4 +11,6 @@ println "${name} has at least ${programmingPoints} programming points."
 println "${programmingPoints} squared is ${square(programmingPoints)}"
 println "${programmingPoints} divided by 2 bonus points is ${divide(programmingPoints, 2)}"
 println "${programmingPoints} minus 7 bonus points is ${subtract(programmingPoints, 7)}"
-println "${programmingPoints} plus 3 bonus points is ${sum(programmingPoints, 3)}"
+println "${programmingPoints} plus 7777777 bonus points is ${sum(programmingPoints, 3)}"
+
+//This is a new feature
